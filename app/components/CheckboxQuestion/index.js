73af03aa61bbabbb627de8db7a889d7b302668fb
lukasz-wasilewski <wasilewskiluke@gmail.com>
2017-02-16 09:37:35 +0100

    fix checkbox question

--- app/components/CheckboxQuestion/index.js
+++ app/components/CheckboxQuestion/index.js
@@ -8,16 +8,9 @@ import React, { PropTypes } from 'react';
 
 export class CheckboxQuestion extends React.Component {
   constructor(props) {
-    super();
+    super(props);
     this.result = [];
-    this.questionId = props.id;
     this.onCheckboxClick = this.onCheckboxClick.bind(this);
-    this.answers = props.answers.map((answer, index) => (
-      <li className="list-element" key={index}>
-        <input type="checkbox" value={answer} onChange={(e) => this.onCheckboxClick(e.target.value, e.target.checked)} /> {answer}
-      </li>
-    ));
-    this.onAnswerChange = props.onAnswerChange;
   }
 
   onCheckboxClick(value, checked) {
@@ -29,13 +22,18 @@ export class CheckboxQuestion extends React.Component {
         this.result.splice(index, 1);
       }
     }
-    this.onAnswerChange(this.questionId, this.result);
+    this.props.onAnswerChange(this.props.id, this.result);
   }
 
   render() {
+    const answers = this.props.answers.map((answer, index) => (
+      <li className="list-element" key={index}>
+        <input type="checkbox" value={answer} onChange={(e) => this.onCheckboxClick(e.target.value, e.target.checked)} /> {answer}
+      </li>
+    ));
     return (
       <ul>
-        {this.answers}
+        {answers}
       </ul>
     );
   }
