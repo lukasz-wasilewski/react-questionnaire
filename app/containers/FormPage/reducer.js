f2d69fc3d710c1738e020854bacef5068afe1db5
lukasz-wasilewski <wasilewskiluke@gmail.com>
2017-02-18 11:55:26 +0100

    fix reducer

--- app/containers/FormPage/reducer.js
+++ app/containers/FormPage/reducer.js
@@ -6,7 +6,6 @@
 
 import { fromJS } from 'immutable';
 import { arrayMove } from 'react-sortable-hoc';
-import _ from 'lodash';
 import {
   GET_FORM_SUCCEED,
   CHANGE_FORM_ORDER,
@@ -26,30 +25,28 @@ const initialState = fromJS({
 function formPageReducer(state = initialState, action) {
   switch (action.type) {
     case GET_FORM_SUCCEED:
-      return state.set('form', action.form);
+      return state.updateIn(['form'], (form) => form.mergeDeep(action.form));
     case CHANGE_FORM_ORDER:
-      return state.set('form', {
-        ...state.toJS().form,
-        questions: arrayMove(state.toJS().form.questions, action.oldIndex, action.newIndex),
-      });
+      return state.updateIn(['form', 'questions'], (questions) =>
+        questions.mergeDeep(arrayMove(questions.toJS(), action.oldIndex, action.newIndex))
+      );
     case DELETE_QUESTION:
-      return state.set('form', {
-        ...state.toJS().form,
-        questions: _.filter(state.toJS().form.questions, (q) => q.id !== action.questionId),
-      });
+      return state.updateIn(['form', 'questions'], (questions) =>
+        questions.filter((q) => q.get('id') !== action.questionId)
+      );
     case QUESTION_ERROR:
-      return state.set('form', {
-        ...state.toJS().form,
-        questions: _.map(state.toJS().form.questions, (q) => q.id === action.id ? { ...q, error: action.ifError } : q),
-      });
+      return state.updateIn(['form', 'questions'], (questions) =>
+        questions.map((q) => q.get('id') === action.id ? q.merge({ error: action.ifError }) : q),
+      );
     case ANSWER_QUESTION:
-      return state.set('answers', {
-        ...state.toJS().answers,
-        [action.id]: {
-          answer: action.answer,
-          questionId: action.id,
-        },
-      });
+      return state.updateIn(['answers'], (answers) =>
+        answers.merge({
+          [action.id]: {
+            answer: action.answer,
+            questionId: action.id,
+          },
+        })
+      );
     default:
       return state;
   }
